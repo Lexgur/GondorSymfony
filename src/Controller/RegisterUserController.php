7a6a0602beee0097b25ef277ac80f8d0ab986076
edgaras <edgaras.malukas@gmail.com>
2025-06-11 01:48:04 +0300

    RegisterUserController added, simple style that is not shown for some reason in dev mode.

--- src/Controller/RegisterUserController.php
+++ src/Controller/RegisterUserController.php
@@ -4,14 +4,16 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class RegisterUserController
+class RegisterUserController extends AbstractController
 {
-    #[Route('/register', name: 'register')]
+    #[Route('/user/register', name: 'register')]
     public function hello(): Response
     {
-        return new Response('Hello World!');
+        return $this->render('user/register.html.twig', [
+        ]);
     }
 }
