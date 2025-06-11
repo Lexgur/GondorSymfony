e0f8d5d772e1d0363aa9732a8bc27b4c37055efb
edgaras <edgaras.malukas@gmail.com>
2025-06-12 00:14:09 +0300

    Migration created, migrated and database connection established.

--- src/Controller/RegisterUserController.php
+++ src/Controller/RegisterUserController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,7 +12,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class RegisterUserController extends AbstractController
 {
     #[Route('/user/register', name: 'register')]
-    public function hello(): Response
+    public function index(EntityManagerInterface $entityManger): Response
     {
         return $this->render('user/register.html.twig', [
         ]);
