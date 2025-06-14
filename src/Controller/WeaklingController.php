eaf2d398f7e32d4a63d092f4c7079dc884eb8f5c
edgaras <edgaras.malukas@gmail.com>
2025-06-14 17:21:53 +0300

    Weakling and Quests controller and their template twigs added.

---
+++ src/Controller/WeaklingController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+final class WeaklingController extends AbstractController
+{
+    #[Route('/user/weakling', name: 'app_weakling')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        if (!$authenticationUtils->getLastUsername()) {
+            throw new AuthenticationException('You must be logged in to view this');
+        }
+
+        return $this->render('user/weakling.html.twig', [
+            'message' => 'You have not completed a single quest? I know a man in white robes, that would be disappointed',
+        ]);
+    }
+}
