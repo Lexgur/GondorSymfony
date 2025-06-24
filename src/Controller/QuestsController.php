6d3d9eae6c1e5c14e27e0f4cdbfe5d948d09affb
edgaras <edgaras.malukas@gmail.com>
2025-06-24 12:14:54 -0400

    Quests controller adjustment made,

--- src/Controller/QuestsController.php
+++ src/Controller/QuestsController.php
@@ -7,7 +7,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 final class QuestsController extends AbstractController
 {
